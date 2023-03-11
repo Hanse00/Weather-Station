1a4072a248c090b2b255ed2bbb6e314050ac8f8c
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2023-03-10 17:33:12 -0800

    Adds middleware for recovery and logging

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/:location", func(c echo.Context) error {
 		location := c.Param("location")
 		js, err := GetWeather(location)
