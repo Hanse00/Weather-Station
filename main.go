7534e6cff6640b7b35d4a4298773d5a9eb719640
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2023-03-24 18:25:07 -0700

    Adds redis caching

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
+	redis_addr := os.Getenv("REDIS_ADDR")
+	if redis_addr == "" {
+		redis_addr = "localhost:6379"
+	}
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redis_addr,
+		Password: "",
+		DB: 0,
+	})
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -16,8 +29,18 @@ func main() {
 
 	e.GET("/observation/:location", func(c echo.Context) error {
 		location := c.Param("location")
-		js, err := GetWeather(location)
-		if err != nil {
+		js, err := rdb.Get(ctx, location).Result()
+		if err == redis.Nil {
+			js, err = GetWeather(location)
+			if err != nil {
+				return err
+			}
+			err := rdb.Set(ctx, location, js, time.Minute).Err()
+			if err != nil {
+				return err
+			}
+			return c.String(http.StatusOK, js)
+		} else if err != nil {
 			return err
 		}
 		return c.String(http.StatusOK, js)
